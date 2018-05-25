f16bc82977f98c0d27adf033f0e48d9f42c43d98
Justine Koa <justinekoa@gmail.com>
2018-05-25 02:09:20 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -4,7 +4,12 @@ def starting_stance
 end
 
 def skip_step(direction)
-  puts "Lift right foot."
-  puts "Return right foot to the ground."
-  puts "Finishing with a small skip-step backward."
+  if direction == "left":
+    puts "Lower left foot to ground."
+    puts "Bounce left foot back up slightly, kicking it a few inches back."
+    puts "Left, Right, Left and Left"
+  elsif direction == "right":
+    puts "Lower right foot to ground."
+    puts "Bounce right foot back up slightly, kicking it a few inches back."
+    puts "Right, Left, Right and Right."
 end
