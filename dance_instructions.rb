fa36563fca9ff2d92a712920721f7503fd2d8176
Justine Koa <justinekoa@gmail.com>
2018-05-25 02:03:20 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -2,3 +2,9 @@
 def starting_stance
   puts "Plant legs far apart, bend knees slightly and keep posture loose."
 end
+
+def skip_step
+  puts "Lift right foot."
+  puts "Return right foot to the ground."
+  puts "Finishing with a small skip-step backward."
+end
