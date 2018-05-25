805538925d08b84e79d07a15ba07acea62dcf579
Justine Koa <justinekoa@gmail.com>
2018-05-25 02:18:01 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -6,5 +6,27 @@ end
 def skip_step(direction)
   puts "Lower left foot to ground."
   puts "Bounce left foot back up slightly, kicking it a few inches back."
-  puts "Left, Right, Left and Left."
+  puts "Briefly Left, Right, Left and Left."
+end
+
+def base_footwork
+  puts "Lift right foot."
+  puts "Return right foot to ground."
+  puts "Finishing with a small skip-step backward."
+end
+
+def bob_the_reins
+  puts "Holding your arms out in front of you, straight and at chest level."
+  puts "Cross your right wrist over your left and hold them together."
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+end
+
+def lasso
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left.."
+  puts "Lift right arm up and point right elbow diagonally to the right."
+  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+end
+
+def bust_a_move
+  puts "Be cool and be snazzy!"
 end
