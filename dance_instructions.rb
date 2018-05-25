1ca1ac80646b689722f3e1b4e9d885de02949696
Justine Koa <justinekoa@gmail.com>
2018-05-25 02:09:27 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -7,7 +7,7 @@ def skip_step(direction)
   if direction == "left":
     puts "Lower left foot to ground."
     puts "Bounce left foot back up slightly, kicking it a few inches back."
-    puts "Left, Right, Left and Left"
+    puts "Left, Right, Left and Left."
   elsif direction == "right":
     puts "Lower right foot to ground."
     puts "Bounce right foot back up slightly, kicking it a few inches back."
